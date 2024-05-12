94ed44cfe49e23954ffb497b56387d6c90f73b8e
kainan <kainanos@outlook.com>
2024-05-12 22:06:58 +0800

    202405121

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AAudioRecorder
+AAudioRecorder is just a demo android app for raw audio capture with AAudio api.
--- app/src/main/cpp/common.h
+++ app/src/main/cpp/common.h
@@ -7,10 +7,10 @@
 
 #include <android/log.h>
 
-#define LOG_TAG  "AAudioPlayer"
+#define LOG_TAG  "AAudioRecorder"
 #define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-#define ALOGW(...) __android_log_print(ANDROID_LOG_WARN,  LOG_TAG, __VA_ARGS__)
-#define ALOGI(...) __android_log_print(ANDROID_LOG_INFO,  LOG_TAG, __VA_ARGS__)
+#define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
 #endif //AAUDIORECORDER_LOG_H
