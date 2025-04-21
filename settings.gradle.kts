1158e3c2189abfcf2dee768a8ba7a06983b3a593
kainan <kainanos@outlook.com>
2025-04-21 12:13:12 +0800

    20250421

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,31 +1,25 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        maven { url = uri("https://maven.aliyun.com/repository/central")}
+        maven { url = uri("https://maven.aliyun.com/repository/public")}
+        maven { url = uri("https://maven.aliyun.com/repository/google")}
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin")}
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter")}
+        google()
         mavenCentral()
-//        maven { url = uri("https://maven.aliyun.com/repository/central")}
-//        maven { url = uri("https://maven.aliyun.com/repository/public")}
-//        maven { url = uri("https://maven.aliyun.com/repository/google")}
-//        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin")}
-//        maven { url = uri("https://maven.aliyun.com/repository/jcenter")}
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/central")}
+        maven { url = uri("https://maven.aliyun.com/repository/public")}
+        maven { url = uri("https://maven.aliyun.com/repository/google")}
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin")}
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter")}
         google()
         mavenCentral()
-//        maven { url = uri("https://maven.aliyun.com/repository/central")}
-//        maven { url = uri("https://maven.aliyun.com/repository/public")}
-//        maven { url = uri("https://maven.aliyun.com/repository/google")}
-//        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin")}
-//        maven { url = uri("https://maven.aliyun.com/repository/jcenter")}
     }
 }
 
